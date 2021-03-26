d97be08e69ffa6111f978193fd00cfd28cec4f4d
Suni711 <78372586+Suni711@users.noreply.github.com>
2021-03-26 10:25:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+var cat ,dog ,girl,hideObject1,hideObject2,hideObject3;
+
+var catImg,dogImg,girlImg,backgroundimg,ideObjImg;
+var hideObjImg2,hideObjImg3;
+
+
+
+function preload(){
+ backgroundImg = loadImage("ground2.png");
+  
+  
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+ 
+}
